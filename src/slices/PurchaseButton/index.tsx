56a419063ec3ec68c0b613b6946c79c6f9378766
Jatesh Parikh <jateshparikh97@gmail.com>
2026-04-08 22:09:45 +0530

    project setup

---
+++ src/slices/PurchaseButton/index.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `PurchaseButton`.
+ */
+export type PurchaseButtonProps =
+  SliceComponentProps<Content.PurchaseButtonSlice>;
+
+/**
+ * Component for "PurchaseButton" Slices.
+ */
+const PurchaseButton: FC<PurchaseButtonProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      Placeholder component for purchase_button (variation: {slice.variation})
+      slices.
+      <br />
+      <strong>You can edit this slice directly in your code editor.</strong>
+      {/**
+       * 💡 Use the Prismic MCP server with your code editor
+       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
+       */}
+    </section>
+  );
+};
+
+export default PurchaseButton;
