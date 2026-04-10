357d994c46ec3708982641ac2853b189f19f1175
Jatesh Parikh <jateshparikh97@gmail.com>
2026-04-10 17:07:57 +0530

    add marquee slice

--- slices/Marquee/index.tsx
+++ slices/Marquee/index.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import { FC, Fragment } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { LogoMark } from "@/components/LogoMark";
+import clsx from "clsx";
 
 /**
  * Props for `Marquee`.
@@ -11,18 +13,42 @@ export type MarqueeProps = SliceComponentProps<Content.MarqueeSlice>;
  * Component for "Marquee" Slices.
  */
 const Marquee: FC<MarqueeProps> = ({ slice }) => {
+  const MarqueeContent = () => (
+    <div className="flex items-center bg-gray-200 py-10 whitespace-nowrap">
+      {slice.primary.phrases.map((item, i) => (
+        <Fragment key={i}>
+          <div className="font-bold-slanted px-14 text-[180px] leading-none text-gray-400/80 uppercase [text-box:trim-both_cap_alphabetic] md:text-[260px]">
+            {item.text}
+          </div>
+          <LogoMark className="size-36 shrink-0" />
+        </Fragment>
+      ))}
+    </div>
+  )
+
   return (
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for marquee (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
+      <div 
+        className="relative flex w-full items-center overflow-hidden select-none"
+        aria-hidden="true"
+        role="presentation"
+      >
+        <div className="relative flex items-center whitespace-nowrap">
+          <div className={clsx(
+            "marquee-track animate-marquee flex", 
+            slice.primary.direction === "Right" && "[animation-direction:reverse]",
+          )}>
+            {/* Content to duplicate */}
+            <MarqueeContent />
+            <MarqueeContent />
+            <MarqueeContent />
+            <MarqueeContent />
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
