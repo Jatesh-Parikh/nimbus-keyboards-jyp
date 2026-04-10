c01c1d60e7c328736d5c2d59ac084f076cbaacd6
Jatesh Parikh <jateshparikh97@gmail.com>
2026-04-10 11:36:17 +0530

    add root page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,27 @@
-import Image from "next/image";
+import { type Metadata } from "next";
+import { notFound } from "next/navigation";
+import { asImageSrc } from "@prismicio/client";
+import { SliceZone } from "@prismicio/react";
 
-export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+import { createClient } from "@/prismicio";
+import { components } from "@/slices";
 
-    </div>
-  );
-}
+export default async function Page() {
+  const client = createClient();
+  const page = await client.getSingle("homepage").catch(() => notFound());
+
+  return <SliceZone slices={page.data.slices} components={components} />
+};
+
+export async function generateMetadata(): Promise<Metadata> {
+  const client = createClient();
+  const page = await client.getSingle("homepage").catch(() => notFound());
+
+  return {
+    title: page.data.meta_title,
+    description: page.data.meta_description,
+    openGraph: {
+      images: [{ url: asImageSrc(page.data.meta_image) ?? "" }]
+    }
+  }
+} 
