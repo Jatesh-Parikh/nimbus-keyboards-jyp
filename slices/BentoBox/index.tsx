8e65765757d4efe7ce51729f7e5c8375fe11c16c
Jatesh Parikh <jateshparikh97@gmail.com>
2026-04-10 15:28:45 +0530

    add BentoBox slice

--- slices/BentoBox/index.tsx
+++ slices/BentoBox/index.tsx
@@ -1,6 +1,10 @@
 import { FC } from "react";
-import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { asText, Content } from "@prismicio/client";
+import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
+import { Bounded } from "@/components/Bounded";
+import { PrismicNextImage } from "@prismicio/next";
+import clsx from "clsx";
+import { FadeIn } from "@/components/FadeIn";
 
 /**
  * Props for `BentoBox`.
@@ -12,19 +16,53 @@ export type BentoBoxProps = SliceComponentProps<Content.BentoBoxSlice>;
  */
 const BentoBox: FC<BentoBoxProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for bento_box (variation: {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+     <FadeIn>
+      <h2 
+        id="features" 
+        className="font-bold-slanted mb-8 scroll-pt-6 text-6xl uppercase md:text-8xl"
+      >
+        <PrismicText field={slice.primary.heading} />
+      </h2>
+     </FadeIn>
+
+     <FadeIn targetChildren className="grid grid-cols-1 gap-4 md:grid-cols-6">
+      {slice.primary.items.map((item) => (
+        <BentoBoxItem key={asText(item.text)} item={item} />
+      ))}
+     </FadeIn>
+    </Bounded>
   );
 };
 
 export default BentoBox;
+
+type BentoBoxItemProps = {
+  item: Content.BentoBoxSliceDefaultPrimaryItemsItem;
+};
+
+function BentoBoxItem({ item }: BentoBoxItemProps) {
+  return (
+    <div className={clsx(
+      "relative overflow-hidden rounded-3xl",
+      item.size === "Small" && "md:col-span-2",
+      item.size === "Medium" && "md:col-span-3",
+      item.size === "Large" && "md:col-span-4"
+    )}>
+      <PrismicNextImage 
+        field={item.image}
+        className="h-full w-full object-cover"
+        quality={96}
+        width={700}
+      />
+
+      <div className="absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-b from-transparent to-black"></div>
+      <div className="absolute bottom-0 left-0 max-w-xl p-6 text-xl text-balance text-white">
+        <PrismicRichText field={item.text} />
+      </div>
+    </div>
+  )
+}
