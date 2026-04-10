a8fce03a0f0244aea2a050ff0d4c4a124e5b8b03
Jatesh Parikh <jateshparikh97@gmail.com>
2026-04-10 17:40:44 +0530

    add ColorChange Scene

--- slices/ColorChanger/index.tsx
+++ slices/ColorChanger/index.tsx
@@ -1,6 +1,49 @@
-import { FC } from "react";
+"use client";
+
+import { FC, useCallback, useState } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, PrismicText, SliceComponentProps } from "@prismicio/react";
+import { Bounded } from "@/components/Bounded";
+import clsx from "clsx";
+import Image from "next/image";
+import { Canvas } from "@react-three/fiber";
+import { Scene } from "./Scene";
+
+export const KEYCAP_TEXTURES = [
+  {
+    id: "goodwell",
+    name: "Goodwell",
+    path: "/goodwell_uv.png",
+    knobColor: "#E44E21",
+  },
+  {
+    id: "dreamboard",
+    name: "Dreamboard",
+    path: "/dreamboard_uv.png",
+    knobColor: "#E9759F",
+  },
+  {
+    id: "cherrynavy",
+    name: "Cherry Navy",
+    path: "/cherrynavy_uv.png",
+    knobColor: "#F06B7E",
+  },
+  { id: "kick", name: "Kick", path: "/kick_uv.png", knobColor: "#FD0A0A" },
+  {
+    id: "oldschool",
+    name: "Old School",
+    path: "/oldschool_uv.png",
+    knobColor: "#B89D82",
+  },
+  {
+    id: "candykeys",
+    name: "Candy Keys",
+    path: "/candykeys_uv.png",
+    knobColor: "#F38785",
+  },
+];
+
+type keycapTexture = (typeof KEYCAP_TEXTURES)[number];
 
 /**
  * Props for `ColorChanger`.
@@ -16,14 +59,7 @@ const ColorChanger: FC<ColorChangerProps> = ({ slice }) => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for color_changer (variation: {slice.variation})
-      slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
+      
     </section>
   );
 };
